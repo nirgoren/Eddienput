4bf042c2312114f2f8a0be79de315103c232ebc4
nirgorentau <nirgoren@mail.tau.ac.il>
2021-02-08 00:49:24 +0200

    Add support for holding buttons Read recordings from a file Add randomization

--- tests/test.py
+++ tests/test.py
@@ -1,97 +1,170 @@
 from pynput.keyboard import Key, Listener, Controller, KeyCode
+from clock import Clock
 import time
-from time import perf_counter
-import pydirectinput
+import random
+from key_emulation import *
 
-# add missing numpad buttons to mapping
-NUMPAD_MAPPING = {
-    'numpad_enter': 0x9C + 1024,
-    'numpad_1': 0x4F,
-    'numpad_2': 0x50,
-    'numpad_3': 0x51,
-    'numpad_4': 0x4B,
-    'numpad_5': 0x4C,
-    'numpad_6': 0x4D,
-    'numpad_7': 0x47,
-    'numpad_8': 0x48,
-    'numpad_9': 0x49,
-    'numpad_0': 0x52
-}
-
-pydirectinput.KEYBOARD_MAPPING.update(NUMPAD_MAPPING)
-
-class Clock:
-
-    def __init__(self, fps):
-        self.start = perf_counter()
-        self.frame_length = 1 / fps
-
-    @property
-    def tick(self):
-        return (perf_counter() - self.start)
-
-    def sleep(self, frame_count):
-        self.start = perf_counter()
-        while self.tick < self.frame_length * frame_count:
-            pass
-            time.sleep(0)
+WAIT_CONST = 'W'
 
 #P2
 
-P = 'numpad_1'
-K = 'numpad_4'
-S = 'numpad_5'
-H = 'numpad_6'
-D = 'numpad_3'
-
-down = 'down'
-left = 'left'
-right = 'right'
-up = 'up'
+# P = 'numpad_1'
+# K = 'numpad_4'
+# S = 'numpad_5'
+# H = 'numpad_6'
+# D = 'numpad_3'
+#
+# down = 'down'
+# left = 'left'
+# right = 'right'
+# up = 'up'
 
 #P1
 
-# P = 'j'
-# K = 'u'
-# S = 'i'
-# H = 'o'
-# D = 'l'
-#
-# down = 's'
-# left = 'a'
-# right = 'd'
-# up = Key.space
+P = 'j'
+K = 'u'
+S = 'i'
+H = 'o'
+D = 'l'
+
+down = 's'
+left = 'a'
+right = 'd'
+up = Key.space
 
 clock = Clock(60)
-keyboard = Controller()
+to_release = set()
+pressed = set()
+
+button_map = {
+    'P': P,
+    'K': K,
+    'S': S,
+    'H': H,
+    'D': D,
+    '2': down,
+    '4': left,
+    '6': right,
+    '8': up
+}
+
+
+def string_to_frames(s: str):
+    moves = []
+    s = s.replace('\n', '')
+    frames = s.split(' ')
+    res = []
+    for frame in frames:
+        if not frame.startswith(WAIT_CONST):
+            res.append(frame)
+        else:
+            if len(frame) == 1:
+                res.append('W')
+            else:
+                # support for Wn with n being a natural number
+                res.extend(['W' for i in range(int(frame[1:]))])
+    s = ' '.join(res)
+    # macros
+    s = s.replace('1', '2+4')
+    s = s.replace('3', '2+6')
+    s = s.replace('7', '4+8')
+    s = s.replace('9', '6+8')
+    s = s.replace('5', 'W')
+    frames = s.split(' ')
+    for frame in frames:
+        frame_moves = []
+        frame = frame.split('+')
+        for button in frame:
+            if button.startswith('W'):
+                pass
+            else:
+                command = 'tap'
+                if button.startswith('['):
+                    command = 'press'
+                    button = button[1:-1]
+                elif button.startswith(']'):
+                    command = 'release'
+                    button = button[1:-1]
+                frame_moves.append((button_map[button], command))
+        moves.append(frame_moves)
+    moves.append([])
+    return moves
+
 
 def wait(frames):
     clock.sleep(frames)
 
-def tap(button):
-    pydirectinput.keyDown(button)
-    wait(1)
-    pydirectinput.keyUp(button)
+
+def reset():
+    clock.reset()
+
 
 def press(button):
-    keyboard.press(button)
+    #t0 = time.perf_counter()
+    press_key(to_key_code(button))
+    #print(time.perf_counter() - t0)
+    print("pressed", button)
+
 
 def release(button):
-    keyboard.release(button)
+    #t0 = time.perf_counter()
+    release_key(to_key_code(button))
+    #print(time.perf_counter() - t0)
+    print("released", button)
+
+
+def run_frame(frame):
+    frame_press_buttons = [x[0] for x in frame if x[1] != 'release']
+    released = set()
+    for button in to_release:
+        if button not in frame_press_buttons:
+            pressed.discard(button)
+            release(button)
+            released.update({button})
+    for button in released:
+        to_release.discard(button)
+    for button, command in frame:
+        if command == 'tap':
+            if button not in pressed:
+                to_release.update({button})
+                pressed.update({button})
+                press(button)
+        elif command == 'press':
+            pressed.update({button})
+            press(button)
+        elif command == 'release':
+            pressed.discard(button)
+            release(button)
+    wait(1)
+
+
+def perform_actions(actions):
+    for frame in actions:
+        run_frame(frame)
+
 
 def do_action():
-    tap(down)
-    wait(40)
-    tap(P)
-    wait(40)
-    tap(D)
-    wait(40)
-    tap(S)
+    i = random.randint(0, len(queues)-1)
+    clock.reset()
+    perform_actions(queues[i])
+
 
 def on_press(key):
-    print(str(key))
+    #print("recieved", str(key))
     if str(key) == "'*'":
         do_action()
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+
+if __name__ == "__main__":
+    queues = []
+    f = open('recordings.txt')
+    for line in f:
+        # ignore comment lines
+        if line.startswith('#'):
+            pass
+        queues.append(string_to_frames(line))
+    # queues.append(string_to_frames('4 5 4 W20 4 5 4 W20 [4] W20 ]4[ 6 5 [6] W17 ]6[ W20 2 3 6 P W13 K+S+H'))
+    # queues.append(string_to_frames('2 3 6 [K] W10 ]K['))
+
+    with Listener(on_press=on_press) as listener:
+        listener.join()
